d4eb15bbee55a9313eec38396a71234e74e97f30
Pamela Argentino <argentinopam@hotmail.com>
2017-03-10 18:57:59 -0300

    create first route

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const app = express();
+const routes = require('./routes');
 
 // App Setup
 app.use(morgan('combined'));
 app.use(bodyParser.json({ type: '*/*' }));
+
+routes(app);
 
 // Server Setup
 const port = process.env.PORT || 3000;
---
+++ routes/index.js
@@ -0,0 +1,7 @@
+module.exports = app => {
+
+  app.get('/', (req, res, next) => {
+    res.send(['waterbottle', 'phone', 'paper']);
+  });
+
+};
